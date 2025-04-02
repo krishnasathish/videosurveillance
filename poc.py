90e779166563fcebf649a3ffc1bd67ee0becb369
Sudarshan Suresh Srikant <sudarshan1994@gmail.com>
2025-04-01 23:44:36 -0400

    Sorted out env errors

--- poc.py
+++ poc.py
@@ -1,32 +1,33 @@
-import streamlit as st
 import os
+import streamlit as st
 from dotenv import load_dotenv
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 from PIL import Image
+import io
 from io import BytesIO
 
-from pyngrok import ngrok
-public_url = ngrok.connect(8502).public_url
-print(f"Public url :  {public_url}")
-
+# Load API key
 load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-client = genai.configure(api_key = GEMINI_API_KEY)
+# Streamlit UI
+st.title("Gemini AI: Text & Image Generator")
 
-st.title("AI Image Generator")
-user_prompt = st.text_input("What do you want to generate image for?")
+option = st.selectbox("Choose output type:", ["Text", "Image"])
+
+prompt = st.text_area("Enter a prompt:")
 
 if st.button("Generate Image"):
-    if not user_prompt:
+    if not prompt:
         st.warning("Please enter the prompt!")
     else:
         try:
             with st.spinner("Generating image..."):
                 response = client.models.generate_content(
                 model="gemini-2.0-flash-exp-image-generation",
-                contents=user_prompt,
-                config=genai.GenerateContentConfig(
+                contents=prompt,
+                config=types.GenerateContentConfig(
                     response_modalities=['Text', 'Image']
                 )
             )
@@ -40,48 +41,4 @@ if st.button("Generate Image"):
                     st.image(image)
                 
         except Exception as e:
-            st.error("Error generating image")
-
-st.title("AI Image Caption Generator")
-
-uploaded_image = st.file_uploader("Upload an image for caption generation", type=["png", "jpg", "jpeg"])
-
-if uploaded_image:
-    image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image")
-
-    if st.button("Generate Caption"):
-        try:
-            with st.spinner("Generating caption..."):
-                response = client.models.generate_content(
-                model="gemini-2.0-flash",
-                contents=["What is this image?", image])
-                st.subheader("Generated Caption:")
-                st.write(response.text)
-        except Exception as e:
-            st.error("Error generating caption")
-
-st.title("AI YouTube Video Summarizer")
-youtube_url = st.text_input("Enter YouTube Video URL")
-
-if st.button("Summarize Video"):
-    if not youtube_url:
-        st.warning("No YouTube URL Present!")
-    else:
-        try:
-            with st.spinner("Generating summary..."):
-                response = client.models.generate_content(
-                    model='models/gemini-2.0-flash',
-                    contents=genai.Content(
-                        parts=[
-                            genai.Part(text='Can you summarize this video?'),
-                            genai.Part(
-                                file_data=genai.FileData(file_uri=youtube_url)
-                            )
-                        ]
-                    )
-                )
-            st.subheader("Video Summary")
-            st.write(response.text)
-        except Exception as e:
-            st.error("Error generating summary")
+            st.error(f"Error generating image {e}")
