52cd190a816f7509274ddb2e0dd06c872e15d7c4
Sudarshan Suresh Srikant <sudarshan1994@gmail.com>
2025-04-02 18:56:07 -0400

    Summarize YT video with timestamp

--- poc.py
+++ poc.py
@@ -14,31 +14,27 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 # Streamlit UI
 st.title("Gemini AI: Text & Image Generator")
 
-option = st.selectbox("Choose output type:", ["Text", "Image"])
+st.title("AI Theft YouTube Video Summarizer")
+youtube_url = st.text_input("Enter YouTube Video URL")
 
-prompt = st.text_area("Enter a prompt:")
-
-if st.button("Generate Image"):
-    if not prompt:
-        st.warning("Please enter the prompt!")
+if st.button("Summarize Video"):
+    if not youtube_url:
+        st.warning("No YouTube URL Present!")
     else:
         try:
-            with st.spinner("Generating image..."):
+            with st.spinner("Anaylizing video..."):
                 response = client.models.generate_content(
-                model="gemini-2.0-flash-exp-image-generation",
-                contents=prompt,
-                config=types.GenerateContentConfig(
-                    response_modalities=['Text', 'Image']
+                    model='models/gemini-2.0-flash',
+                    contents=types.Content(
+                        parts=[
+                            types.Part(text='Can you highlight any suspicious behavior and provide the timestamp?'),
+                            types.Part(
+                                file_data=types.FileData(file_uri=youtube_url)
+                            )
+                        ]
+                    )
                 )
-            )
-            
-            st.subheader("Generated Image")
-            for part in response.candidates[0].content.parts:
-                if part.text is not None:
-                    st.write(part.text)
-                elif part.inline_data is not None:
-                    image = Image.open(BytesIO((part.inline_data.data)))
-                    st.image(image)
-                
+            st.subheader("Video analysis Summary")
+            st.write(response.text)
         except Exception as e:
-            st.error(f"Error generating image {e}")
+            st.error("Error generating summary")
